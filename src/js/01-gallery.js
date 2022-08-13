c7b22e61e97f6be028bcd18d8ea4ec52a42aceab
Yurii-Bodnar <yuriy.bodnar75@gmail.com>
2022-08-13 15:58:42 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+import SimpleLightbox from "simplelightbox";
+
+const galleryRef = document.querySelector(".gallery");
+
+
+
+const galleryItem = galleryItems.map(gallery => 
+    `<div class="gallery__item">
+        <a class="gallery__item" href="${gallery.original}">
+        <img
+         class="gallery__image"
+         src="${gallery.preview}"
+         data-source="${gallery.original}" 
+         alt="${gallery.description}"
+         />
+        </a>
+     </div>`).join("")
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryItem);
+const lightbox = new SimpleLightbox('.gallery div a' , { captionsData: "alt",captionDelay: 250});
