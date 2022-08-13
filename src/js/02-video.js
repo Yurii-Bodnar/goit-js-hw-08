90e3a448f48ec39bac5d52979d0ad6a195bb1ebe
Yurii-Bodnar <yuriy.bodnar75@gmail.com>
2022-08-13 23:57:59 +0300

    Add lib

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player'
+const iframe = document.querySelector("#vimeo-player");
+    const player = new Player(iframe);
+
+    player.on('timeupdate',(data)=> localStorage.setItem('videoplayer-current-time', data.seconds))
+
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) { }).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+    });
 
